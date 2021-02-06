3f8a60fb1fb5b3702ea36f3bccae044234bb9ed0
yu-croco <yuki.kita22@gmail.com>
2021-02-06 20:16:30 +0900

    remove unused file

--- resource/iam/var.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  listAllBuckets = "s3:ListAllMyBuckets"
-  getAllBuckets = "s3:GetBucketLocation"
-  targetS3Path = "arn:aws:s3:::sample-tf/*"
-}
--- resource/iam2/var.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  listAllBuckets = "s3:ListAllMyBuckets"
-  getAllBuckets = "s3:GetBucketLocation"
-  targetS3Path = "arn:aws:s3:::sample-tf/*"
-}
-
-variable "iam1_role_name" {
-  type = string
-}
